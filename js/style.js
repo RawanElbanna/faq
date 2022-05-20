0584710b8ccff22e23c00429afc8fe82fb87a4e2
RawanElbanna <99205603+RawanElbanna@users.noreply.github.com>
2022-05-20 13:34:18 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,1 +1,8 @@
-
+const accItems = document.querySelectorAll(".item");
+accItems.forEach((acc) => acc.addEventListener("click", toggleAcc));
+function toggleAcc() {
+  accItems.forEach((item) => item != this ? item.classList.remove("item--active") : null);
+  if (this.classList != "item--active") {
+    this.classList.toggle("item--active");
+  }
+}
